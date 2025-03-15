cc7c18824ab89b8c8b4f954d337f506690bba944
Renad 10 <116083098+Renad10@users.noreply.github.com>
2025-03-15 18:55:08 +0100

    youtube video embed

--- my_app/models.py
+++ my_app/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-
+import re
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
-
-from django.db import models
 
 class Project(models.Model):
     # Opties voor projecttype
@@ -21,14 +19,24 @@ class Project(models.Model):
     tags = models.ManyToManyField('Tag', related_name="projects")
     type = models.CharField(max_length=20, choices=PROJECT_TYPES, default='personal')  # Type van het project
     link = models.URLField(blank=True, null=True)  # Optionele link naar het project
+    video_url = models.URLField(blank=True, null=True)  # Optionele YouTube-video
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-    
+
     def poster_image(self):
         return self.images.filter(is_poster=True).first()
 
+    def embed_video(self):
+        """Converteert een YouTube-link (normaal of Shorts) naar een embed-link"""
+        if self.video_url:
+            # Zoek naar een normale YouTube-video of een Shorts-link
+            match = re.search(r"(?:v=|/shorts/)([^&/?]+)", self.video_url)
+            if match:
+                return f"https://www.youtube.com/embed/{match.group(1)}"
+        return None
 
 class ProjectImage(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="images")
