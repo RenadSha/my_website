7af60f2e5083c9f8ecdf8b1c09d6efccaa05f242
Renad 10 <116083098+Renad10@users.noreply.github.com>
2025-03-15 15:47:30 +0100

    some changes

--- my_app/views.py
+++ my_app/views.py
@@ -4,4 +4,5 @@ from .models import Project
 # Create your views here.
 def index(request):
     projects = Project.objects.all().prefetch_related("images")
+  
     return render(request, "index.html", {'projects':projects})
